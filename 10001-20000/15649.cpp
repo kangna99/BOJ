35e262ff4a88561df70875cf069ca535ccad6275
nayooniverse <65649518+kangna99@users.noreply.github.com>
2022-07-19 23:34:38 +0800

    [백트래킹] N과 M (1)

--- 10001-20000/15649.cpp
+++ 10001-20000/15649.cpp
@@ -6,18 +6,18 @@ int arr[MAX];
 bool visited[MAX];
 
 void dfs(int depth) {
-	if(depth == M) {
+	if(depth == M) { //배열 다 채우면 출력
 		for(int i=0; i<M; i++)
 			cout << arr[i] << " ";
 		cout << '\n';
 	}
 	else {
 		for(int i=1; i<=N; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				arr[depth] = i;
-				dfs(depth+1);
-				visited[i] = false;
+			if(!visited[i]) { //아직 방문하지 않은 수라면
+				visited[i] = true; //방문 기록
+				arr[depth] = i; //배열에 넣음
+				dfs(depth+1); //다음 수 정하기
+				visited[i] = false; //방문 기록 지움
 			}
 		}
 	}
@@ -26,6 +26,6 @@ void dfs(int depth) {
 int main(void) {
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
 	cin >> N >> M;
-	dfs(0);
+	dfs(0); //처음(0번째) 수 정하기
 	return 0;
 }
