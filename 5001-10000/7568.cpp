df696136e62b8ba49fb627096db5037d5d06449f
nayooniverse <65649518+kangna99@users.noreply.github.com>
2023-01-07 15:17:01 +0900

    [구현] 덩치

---
+++ 5001-10000/7568.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, x, y;
+vector<pair<int, int>> v;
+int ans[50];
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> x >> y;
+        v.push_back({x, y}); 
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j) continue;
+            if(v[i].first < v[j].first && v[i].second < v[j].second) {
+                ans[i]++;
+            }
+        }
+    }
+    
+    for(int i = 0; i < n; i++) {
+        cout << ans[i]+1 << ' ';
+    }
+}
