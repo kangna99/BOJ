19d681f46749f8c32bfc5c0bc5d8a8685763f860
nayooniverse <65649518+kangna99@users.noreply.github.com>
2023-02-08 17:25:48 +0900

    [정렬] 단어 정렬
    1) 버킷 정렬 풀이
    2) sort + compare 함수 custom 풀이

--- 1000-5000/1181.cpp
+++ 1000-5000/1181.cpp
@@ -3,21 +3,46 @@
 #include <algorithm>
 using namespace std;
 
+// int n;
+// string str;
+// vector<string> v[51];
+
+// int main() {
+//     ios::sync_with_stdio(false); cin.tie(nullptr);
+//     cin >> n;
+//     for(int i = 0; i < n; i++) {
+//         cin >> str;
+//         v[str.size()].push_back(str);
+//     }
+//     for(auto &s: v) {
+//         sort(s.begin(), s.end());
+//         s.erase(unique(s.begin(), s.end()), s.end());
+//         for(string const &ss: s)
+//             cout << ss << '\n';
+//     }
+// }
+
 int n;
 string str;
-vector<string> v[51];
+vector<string> v;
+
+bool compare(const string &a, const string &b) {
+    if (a.size() != b.size()) return a.size() < b.size();
+    return a < b;
+}
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
     cin >> n;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> str;
-        v[str.size()].push_back(str);
+        v.push_back(str);
     }
-    for(auto s: v) {
-        sort(s.begin(), s.end());
-        s.erase(unique(s.begin(), s.end()), s.end());
-        for(string ss: s)
-            cout << ss << '\n';
+
+    sort(v.begin(), v.end(), compare);
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    for (auto const &s : v) {
+        cout << s << '\n';
     }
 }
