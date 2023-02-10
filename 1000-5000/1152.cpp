98aeb979eef82ba1ed5cb561633aac0b4f5f97e2
nayooniverse <65649518+kangna99@users.noreply.github.com>
2023-02-10 23:35:12 +0900

    [구현] 단어의 개수

---
+++ 1000-5000/1152.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    string str;
+    int ans = 0;
+    while(cin >> str) ans++;
+    cout << ans;
+}
